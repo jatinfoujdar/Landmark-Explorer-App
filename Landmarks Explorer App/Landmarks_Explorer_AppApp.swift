8d3552b9c98e8486aaa5fbe1bc8c944b114c8414
Jatin Foujdar <jatin321001@gmail.com>
2024-10-20 21:13:50 +0530

    Initial Commit

---
+++ Landmarks Explorer App/Landmarks_Explorer_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Landmarks_Explorer_AppApp.swift
+//  Landmarks Explorer App
+//
+//  Created by jatin foujdar on 20/10/24.
+//
+
+import SwiftUI
+
+@main
+struct Landmarks_Explorer_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
