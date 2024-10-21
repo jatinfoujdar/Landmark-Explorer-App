354d201c27b326e4a5faf793b239b782a05943d2
Jatin Foujdar <jatin321001@gmail.com>
2024-10-21 21:10:34 +0530

    creating combining view commit

--- Landmarks Explorer App/ContentView.swift
+++ Landmarks Explorer App/ContentView.swift
@@ -9,11 +9,16 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
+        VStack(alignment: .leading) {
             Text("Turtle Rock")
                 .font(.title)
-                .foregroundStyle(.green)
-            
+            HStack{
+                Text("Joshua Tree National park")
+                    .font(.subheadline)
+                Spacer()
+                Text("California")
+                    .font(.subheadline)
+            }
         }
         .padding()
     }
