bf44a681cb22186d43f31f254e99d63cdfe3e1cd
Jatin Foujdar <jatin321001@gmail.com>
2024-10-25 15:23:38 +0530

    create fav button commit

---
+++ Landmarks Explorer App/View/FavoriteButton.swift
@@ -0,0 +1,19 @@
+//
+//  FavoriteButton.swift
+//  Landmarks Explorer App
+//
+//  Created by jatin foujdar on 25/10/24.
+//
+
+import SwiftUI
+
+struct FavoriteButton: View {
+    @Binding var isSet: Bool
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FavoriteButton(isSet: .constant(true))
+}
